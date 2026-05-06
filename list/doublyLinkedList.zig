3df16bc347bd7f8e9a78104411b2d666deb2ed04
Watashi00 <nova2948@gmail.com>
2026-05-06 11:46:31 -0300

    feat: added tail to reduce complexity to O(1) on append.

--- list/doublyLinkedList.zig
+++ list/doublyLinkedList.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
 
-const Node = @import("../ds/node.zig");
+const nodeModule = @import("../ds/node.zig");
+const Node = nodeModule.Node;
 
 pub const doublyLinkedList = struct {
     head: ?*Node,
+    tail: ?*Node,
     length: usize,
     allocator: std.mem.Allocator,
 
@@ -15,39 +17,54 @@ pub const doublyLinkedList = struct {
         };
     }
 
-    pub fn createNode(self: *doublyLinkedList, content: []const u8 ) !void {
+    pub fn append(self: *doublyLinkedList, content: []const u8) !void {
         const new_node = try self.allocator.create(Node);
-        
+
         new_node.* = Node{
             .payload = content,
             .next = null,
-            .prev = null
+            .prev = null,
         };
 
         if (self.head == null) {
             self.head = new_node;
-            self.resizeLen(1);
-            return;
-        } 
-        if(self.head.?.next == null) {
-            self.head.?.next = new_node;
-            new_node.prev = self.head;
+            self.tail = new_node;
             self.resizeLen(1);
             return;
         }
-        
-        var cur = self.head.?.next;
-        while(cur.?.next != null) {
-            cur = cur.?.next;
-        }
-        cur.?.next = new_node;
+
+        new_node.prev = self.tail;
+        self.tail.?.next = new_node;
+        self.tail = new_node;
+
         self.resizeLen(1);
-        return;
-        
     }
 
-    fn resizeLen(self: *doublyLinkedList, qtd: i32) void {
-        self.length += qtd;
+    pub fn pop() ?*Node {
+
     }
 
+    pub fn remove() !void  {
+
+    }
+
+    fn incLen(self: *doublyLinkedList) void {
+        self.length += 1;
+    }
+
+    fn decLen(self: *doublyLinkedList) void {
+        if (self.length == 0) {
+            @panic("length underflow");
+        }
+        self.length -= 1;
+    }
+
+    pub fn deinit(self: *doublyLinkedList) void {
+        var cur = self.head;
+        while(cur) |node| {
+            const next = node.next;
+            self.allocator.destroy(node);
+            cur = next;
+        }
+    }
 };
