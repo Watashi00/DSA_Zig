7f7cf1f20a1099455295c3baaecb277515283700
Watashi00 <nova2948@gmail.com>
2026-05-06 22:48:15 -0300

    feat: created new functions, created helpers and refactored

--- list/doublyLinkedList.zig
+++ list/doublyLinkedList.zig
@@ -10,12 +10,7 @@ pub const doublyLinkedList = struct {
     allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator) doublyLinkedList {
-        return doublyLinkedList {
-            .head = null,
-            .tail = null,
-            .length = 0,
-            .allocator = allocator
-        };
+        return doublyLinkedList{ .head = null, .tail = null, .length = 0, .allocator = allocator };
     }
 
     pub fn append(self: *doublyLinkedList, content: []const u8) !void {
@@ -42,33 +37,77 @@ pub const doublyLinkedList = struct {
     }
 
     pub fn pop(self: *doublyLinkedList) ?*Node {
-        if(self.tail == null) return;
-        const retNode = self.tail;
-
-        if(retNode.?.prev) |prev| {
-            prev.next = null;
-            self.tail = prev;
-        } else {
-            self.head = null;
-            self.tail = null;
-        }
-        self.decLen();
+        if (self.tail == null) return null;
+        const retNode = self.tail.?;
+        self._remove(retNode);
         return retNode;
     }
+
+    pub fn getByIndex(self: *doublyLinkedList, index: usize) ?*Node {
+        if (self.head == null or index >= self.length) return null;
+        if (index < self.length / 2) {
+            var cur = self.head;
+            var i: usize = 0;
+            while (cur) |node| {
+                if (i == index) return node;
+                cur = node.next;
+                i += 1;
+            }
+        } else {
+            var cur = self.tail;
+            var i: usize = self.length - 1;
+            while (cur) |node| {
+                if (i == index) return node;
+                cur = node.prev;
+                
+                if (i == 0) break;
+                i -= 1;
+            }
+        }
+        return null;
+    }
+
+    pub fn popByIndex(self: *doublyLinkedList, index: usize) ?*Node {
+        if (self.head == null) return null;
+        const retNode = self.getByIndex(index);
+        if (retNode) |node| {
+            self._remove(node);
+        }
+        return retNode;
+    }
+
     pub fn printList(self: *doublyLinkedList) !void {
-        if(self.head == null) return;
+        if (self.head == null) return;
 
         var cur = self.head;
 
-        while(cur) |node|{
+        while (cur) |node| {
             std.debug.print("{s}", .{node.payload});
             if (node.next != null) std.debug.print(" <-> ", .{});
             cur = node.next;
         }
     }
 
-    pub fn remove(self: *doublyLinkedList) !void  {
-        self.pop();
+    pub fn remove(self: *doublyLinkedList) void {
+        if (self.tail) |tailNode| {
+            self._remove(tailNode);
+            self.allocator.destroy(tailNode);
+        }
+    }
+
+    fn _remove(self: *doublyLinkedList, node: *Node) void {
+        if (node.prev) |prev| {
+            prev.next = node.next;
+        } else {
+            self.head = node.next;
+        }
+
+        if (node.next) |next| {
+            next.prev = node.prev;
+        } else {
+            self.tail = node.prev;
+        }
+        self.decLen();
     }
 
     pub fn getHead(self: *doublyLinkedList) ?*Node {
@@ -92,7 +131,7 @@ pub const doublyLinkedList = struct {
 
     pub fn deinit(self: *doublyLinkedList) void {
         var cur = self.head;
-        while(cur) |node| {
+        while (cur) |node| {
             const next = node.next;
             self.allocator.destroy(node);
             cur = next;
